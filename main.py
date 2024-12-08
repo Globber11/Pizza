8242b1eb1689cb32e3469df5c8cb1e52bc09c648
ViperrIvan <smokerskatedik@gmail.com>
2024-12-08 22:29:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from controller import openPizza
-if __name__ == "__main__":
-    openPizza()
+from controller import openPizza    # импорт функции запускающей всю программу
+if __name__ == "__main__":   # если имя фвйла равно main запускаем программу
+    openPizza()  # запуск программы
